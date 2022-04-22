b64516973788797ebb5d555392ad100e0d4d6f5a
sheyko <sheyko.wk@gmail.com>
2022-04-21 21:47:10 -0500

    add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import getAllTodos from './services/getAllTodos';
 import TodoItem from './components/TodoItem';
+import { useForm } from 'react-hook-form';
 
 function App() {
 
+  const {register, handleSubmit, reset} = useForm()
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
     getAllTodos()
       .then((response) => {
+        console.log(response.data)
         setTodos(response.data.todos)
       })
   }, [])
+
+  const onSubmit = (res) => {
+    console.log(res)
+  }
 
   const list = todos.map((todo) => <TodoItem key={todo.id} todoObj={todo} />)
 
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h3>Estudiante</h3>
+          <input {...register("student")} />
+          <h3>Tarea</h3>
+          <input {...register("task")} />
+          <button>Enviar</button>
+        </form>
         {list}
       </header>
     </div>
